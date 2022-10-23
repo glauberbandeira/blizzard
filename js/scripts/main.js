a0f515f5e474c1cff29a2835097678ceef6180dc
Glauber Bandeira <glauberbandeira_@hotmail.com>
2022-10-23 21:16:43 +1100

    finished slide

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,23 @@
+var slide_thumbnail = new Swiper(".slide-thumbnail", {
+  // Mostrar 05 slides
+  slidesPerView: 5,
+  // Deixar na vertical
+  direction: 'vertical',
+  // Espaçamento entrar os icones
+  spaceBetween: 20,
+  // Assistindo a alteração do outro slide
+  watchSlidesProgress: true
+});
+
 var slide_hero = new Swiper(".slide-principal", {
-  effect: 'fade'
+  effect: 'fade',
+  // Passar um opção no objeto thumbs
+  thumbs: {
+    swiper: slide_thumbnail,
+  },
+  // Deixar automatico
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  }
 });
