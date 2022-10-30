ce46ec7b9ce75151b103fdb64a97e66ab9776730
Glauber Bandeira <glauberbandeira_@hotmail.com>
2022-10-30 21:58:30 +1100

    menu-dropdown + js finished

--- js/main.js
+++ js/main.js
@@ -61,3 +61,34 @@ btnFecharMOdal.addEventListener('click', () => {
   let tagHtml = document.documentElement;
   tagHtml.classList.remove('show-modal');
 })
+
+// Menu dropdown
+const btnMenu = document.querySelectorAll('.js-btn-menu');
+const menuSite = document.querySelectorAll('.js-menu')
+
+btnMenu.forEach((btn, index) => {
+  btn.addEventListener('click', (event) => {
+    // negar o evento de a
+    event.preventDefault();
+
+    menuSite.forEach(itemMenu => {
+      itemMenu.classList.remove('active')
+      // retirar o mouse, feche o menu
+      itemMenu.addEventListener('mouseleave', () => {
+        itemMenu.classList.remove('active')
+        btnMenu.forEach(itemBtn => {
+          itemBtn.classList.remove('active')
+        })
+      })
+    })
+
+    btnMenu.forEach(itemBtn => {
+      itemBtn.classList.remove('active')
+    })
+
+    btn.classList.add('active');
+    menuSite[index].classList.add('active');
+  })
+
+
+})
